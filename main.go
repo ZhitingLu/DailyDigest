5b1dc54b11fe1873cc0c6f47bd792f353d6f8d86
ZhitingLu <zhitinglu@outlook.com>
2023-06-12 21:27:09 +0200

    Added news api

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"net/url"
 	"html/template"
+	"fmt"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -15,6 +18,25 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
   }
   
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
+}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -27,10 +49,19 @@ func main() {
 		port = "3000"
 	}
 
-	mux := http.NewServeMux()
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apikey must be set")
+	}
 
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi := news.NewClient(myClient, apiKey, 20)
+
+	fs := http.FileServer(http.Dir("assets"))
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/search/", searchHandler(newsapi))
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
-
-
